1d7f8b74856a2f3181d76a18acf2ed1efccfd90a
devkumarnishad23-beep <devkumarnishad23@gmail.com>
2026-08-25 18:44:16 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk { version = release(34) { minorApiLevel = 1 } }
 
   defaultConfig {
     applicationId = "com.aistudio.jaibajrangakhada.snpl"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 
